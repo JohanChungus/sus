e388be0c6e22fe981f17df492a50e0aecb2ec3f6
YTTA YANG TAHU TAHU AJA <113217515+JohanChungus@users.noreply.github.com>
2024-01-13 17:37:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import os
 
 api_id = '21934531'
 api_hash = '09913d693f06928ddfce1d1ec04f5f2c'
-photos_directory = 'yes/'
+photos_directory = 's/'
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 def change_profile_photo():
-    photos = os.listdir(photos_directory)
-    for photo in photos:
-        with app:
-            app.set_profile_photo(photo=photos_directory + '/' + photo)
-        time.sleep(60)  # W
+    while True:  # Loop tanpa batas
+        photos = os.listdir(photos_directory)
+        for photo in photos:
+            with app:
+                app.set_profile_photo(photo=photos_directory + '/' + photo)
+            time.sleep(60)  # Tunggu selama 60 detik
 
 if __name__ == "__main__":
     change_profile_photo()
